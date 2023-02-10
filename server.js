b67aa460d1b47daab4c2836f07f64138717b6a27
Hunter Andersen <andersen.hunter42@gmail.com>
2023-02-10 11:38:53 -0800

    Finished exercise

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import config from "./config";
 // TODO: import router from routes/
+import router from "./routes"
 
 const app = express();
 
+//http://localhost:5000/api/test
+
+//Middleware
 app.use(express.json());
+app.use("/api", router);
 
 // TODO: use the imported router to handle all requests
 
+//Error handler middleware
 app.use((err, req, res, next) => {
   console.error(err);
   res.json({ name: err.name, msg: err.message });
